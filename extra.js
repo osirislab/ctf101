4e6fe626b8ca99bd9d4aae1642b0e5e07e6759d7
Kevin Chung <kchung@nyu.edu>
2018-05-10 19:12:55 -0500

    Deployed b151a49 with MkDocs version: 0.17.3

---
+++ extra.js
@@ -0,0 +1,9 @@
+$('.dropdown-submenu > a').click(function(e){
+    var menu = $(this);
+    if (menu.attr('href') == '#'){
+        var submenu = menu.parent()
+        var first_link = submenu.find('.dropdown-menu a')[0]
+        console.log(first_link)
+        first_link.click();
+    }
+});
